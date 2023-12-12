736954037ab212cfcd22223213cc3e8c29f881b3
nada <nadayahya2020@hotmail.com>
2023-12-12 12:39:28 -0800

    Done fix mongoDB

--- configs/index.js
+++ configs/index.js
@@ -2,9 +2,9 @@ import 'dotenv/config'
 
 export const dev = {
   app: {
-    port: process.env.PORT ,
+    port: process.env.PORT || 2007,
   },
   db: {
-    url: process.env.MONGODB_URL || 'mongodb+srv://nadaYahya:Almalki123@cluster0.tvnc3qb.mongodb.net/awsDB',
+    url: process.env.MONGODB_URL || 'mongodb+srv://nadaYahya:Almalki123@cluster0.tvnc3qb.mongodb.net/awsDB?retryWrites=true&w=majority',
   },
 }
